70eb7a4eeb4307eae7ffdb4e346c35dc3108918c
royendo <m.royendo@gmail.com>
2024-05-08 16:45:02 +0900

    test

--- models/airline_stocks_model.sql
+++ models/airline_stocks_model.sql
@@ -1,4 +1,14 @@
 -- Model SQL
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
-select * from airline_stocks
+select "Ticker",
+CAST("Open" as DOUBLE) as "Open",
+CAST("High" as DOUBLE) as "High",
+CAST("Low" as DOUBLE) as "Low",
+cast("Close" as DOUBLE)  as "Close",
+cast("Adj Close" as DOUBLE) as "Adj Close",
+cast("Volume" as INT) as "Volume",
+cast("Date" as DATE) as "Date"
+ 
+from airline_stocks 
+
