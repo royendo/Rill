035b4a80c9bdf824c3d74e8b03d0590a08aee5f4
royendo <m.royendo@gmail.com>
2024-05-08 12:50:35 +0900

    cool

--- models/model.sql
+++ models/model.sql
@@ -1,4 +1,17 @@
 -- Model SQL
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
-SELECT * from crm_history_gcs JOIN web_new_customers_gcs ON (crm_history_gcs.customer_id = web_new_customers_gcs.customer_id)
+
+
+SELECT t1.*, t2.age, t2.gender, t2.revenue, t2.purchased
+FROM web_history_gcs t1
+JOIN (
+    SELECT
+        EXTRACT(YEAR FROM age(current_date(), birth)) AS age,
+        customer_id,
+        gender,
+        revenue,
+        purchased
+    FROM crm_history_gcs
+) t2 ON t1.customer_id = t2.customer_id
+WHERE t2.age <= 100;
