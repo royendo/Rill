8cc8038692e4a20e1f860f212d013b8a89b113f6
royendo <m.royendo@gmail.com>
2024-05-09 10:25:34 +0900

    testing

--- models/crm_history_gcs_model_1.sql
+++ models/crm_history_gcs_model_1.sql
@@ -1,8 +1,4 @@
 -- Model SQL
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
-select *
- from airline_stocks 
-{{ if dev }} LIMIT 100 {{ end }}
-
-
+select * from crm_history_gcs
