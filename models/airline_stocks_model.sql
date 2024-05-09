8236ce1dcda4b48e279ba8207a829a1b6a509856
royendo <m.royendo@gmail.com>
2024-05-09 10:02:16 +0900

    test

--- models/airline_stocks_model.sql
+++ models/airline_stocks_model.sql
@@ -1,7 +1,8 @@
 -- Model SQL
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
-select * from airline_stocks 
+select CAST("Date" as DATE) as date_column, "TICKER", "Open", "High", "Low", "Close", "Adj Close", "Volume"
+ from airline_stocks 
 {{ if dev }} LIMIT 100 {{ end }}
 
 
