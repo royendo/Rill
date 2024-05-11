a2ce44c123cea4c2481731ebc04b1c3d98fd9092
royendo <m.royendo@gmail.com>
2024-05-11 22:29:54 +0900

    ?

--- models/Other /airline_stocks_model_1.sql
+++ models/Other /airline_stocks_model_1.sql
@@ -1,8 +1,7 @@
 -- Model SQL
 -- Reference documentation: https://docs.rilldata.com/reference/project-files/models
 
-select *
- from airline_stocks 
+select * from airline_stocks 
 -- {{ if dev }} LIMIT 100 {{ end }}
 
 
