7f2d15b55928d37e4a734758968806e62891325e
roy endo <m.royendo@gmail.com>
2024-05-18 08:46:37 +0900

    without the data

---
+++ models/CH.sql
@@ -0,0 +1,7 @@
+-- Model SQL
+-- Reference documentation: https://docs.rilldata.com/reference/project-files/models
+
+select * from default.Coffee_domestic_consumption;
+-- {{ if dev }} LIMIT 100 {{ end }}
+
+
---
+++ models/daily_weather_preprocessed_1896_2023_model.sql
@@ -0,0 +1,4 @@
+-- Model SQL
+-- Reference documentation: https://docs.rilldata.com/reference/project-files/models
+
+select * from daily_weather_preprocessed_1896_2023
